8151f62924166635ae559784f167a3e2e776aba1
REHYLIN <rehylinm@gamil.com>
2026-01-11 10:01:04 -0400

    HELLO!!

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
 import pygame, sys
 
 pygame.init()
+
 ANCHO, ALTO = 800, 600
 PISO_Y = 480
 
-
 pantalla = pygame.display.set_mode((ANCHO, ALTO))
 pygame.display.set_caption("foxes adventure")
+
+clock = pygame.time.Clock()
+
 fondo = pygame.image.load("img/FondoDeTutorial.png").convert()
 fondo = pygame.transform.scale(fondo, (ANCHO, ALTO))
-clock = pygame.time.Clock()
+
 
 class Jugador:
     def __init__(self):
-
         self.idle = pygame.image.load("img/Fox.png").convert_alpha()
-        self.idle = pygame.transform.scale(self.idle, (135, 135))
+        self.idle = pygame.transform.scale(self.idle, (155, 155))
+
+        self.salto_img = pygame.image.load("img/salto1.png").convert_alpha()
+        self.salto_img = pygame.transform.scale(self.salto_img, (135, 135))
 
         self.animaciones_correr = []
-        for i in range(1, 7):
-
-
-            img = pygame.image.load("img/inestavilidad.png").convert_alpha()
-            img = pygame.transform.scale(img, (135, 135))
-            self.animaciones_correr.append(img)
-
+        for i in range(1, 4):
             img = pygame.image.load("img/correr2.png").convert_alpha()
             img = pygame.transform.scale(img, (135, 135))
             self.animaciones_correr.append(img)
@@ -41,9 +40,8 @@ class Jugador:
             img = pygame.transform.scale(img, (135, 135))
             self.animaciones_correr.append(img)
 
-
-        self.frame = 0
         self.image = self.idle
+        self.frame = 0
 
         self.rect = self.image.get_rect(midbottom=(ANCHO // 2, PISO_Y))
 
@@ -74,6 +72,7 @@ class Jugador:
         if keys[pygame.K_UP] and self.en_suelo:
             self.vel_y = self.salto
             self.en_suelo = False
+
         self.vel_y += self.gravedad
         self.rect.y += self.vel_y
 
@@ -84,11 +83,15 @@ class Jugador:
 
         ahora = pygame.time.get_ticks()
 
-        if moviendo:
+        if not self.en_suelo:
+            self.image = self.salto_img
+
+        elif moviendo:
             if ahora - self.last_update > self.anim_delay:
                 self.frame = (self.frame + 1) % len(self.animaciones_correr)
                 self.last_update = ahora
             self.image = self.animaciones_correr[self.frame]
+
         else:
             self.frame = 0
             self.image = self.idle
